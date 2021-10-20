c06d1f35761848753df452a7803f90bc75e67197
Scaletts <duongtruongtho12@gmail.com>
2021-10-20 14:39:41 +0700

    update

---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+module.exports.Token = require('./token.model');
+module.exports.User = require('./user.model');
+module.exports.Lop = require('./lop.model');
+module.exports.Sinhvien = require('./sinhvien.model');
+module.exports.ApparelSize = require('./apparelSize.model');
+module.exports.Product = require('./product.model');
+module.exports.ProductCategories = require('./productCategories.model');
+module.exports.People = require('./people.model');
+module.exports.Professional = require('./professional.model');
+module.exports.Worksfor = require('./worksfor.model');
+module.exports.Companies = require('./companies.model');
+
---
+++ src/routes/v1/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const authRoute = require('./auth.route');
+const userRoute = require('./user.route');
+const docsRoute = require('./docs.route');
+const lopRoute = require('./lop.route');
+const sinhvienRoute = require('./sinhvien.route');
+const apparelSizeRoute = require('./apparelSize.route');
+const productRoute = require('./product.route');
+const productCategoriesRoute = require('./productCategories.route');
+const peopleRoute = require('./people.route');
+const professionalRoute = require('./professional.route');
+const worksforRoute = require('./worksfor.route');
+const companiesRoute = require('./companies.route');
+const config = require('../../config/config');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/users',
+    route: userRoute,
+  },
+  {
+    path: '/lops',
+    route: lopRoute,
+  },
+  {
+    path: '/sinhviens',
+    route: sinhvienRoute,
+  },
+  {
+    path: '/apparelsizes',
+    route: apparelSizeRoute,
+  },
+  {
+    path: '/products',
+    route: productRoute,
+  },
+  {
+    path: '/productcategoriess',
+    route: productCategoriesRoute,
+  },
+  {
+    path: '/peoples',
+    route: peopleRoute,
+  },
+  {
+    path: '/professionals',
+    route: professionalRoute,
+  },
+  {
+    path: '/worksfors',
+    route: worksforRoute,
+  },
+  {
+    path: '/companiess',
+    route: companiesRoute,
+  },
+];
+
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
+
+module.exports = router;
